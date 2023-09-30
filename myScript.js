0ee6766447af44f231f82d6f90de7703c042c703
alexandru <109861946+magic-doggo@users.noreply.github.com>
2023-09-30 12:31:42 +0000

    Create function for a single game of RPS against pc

--- myScript.js
+++ myScript.js
@@ -11,3 +11,42 @@ console.log(getComputerChoice())
 // return a string that declares the winner of the round
 // make player parameter case-insensitive
 
+function playRPS (playerSelection, computerSelection){
+  if (playerSelection == "rock"){
+    if (computerSelection =="rock") {
+      return "Rock Draw"
+    }
+    else if (computerSelection == "paper") {
+      return "You Lose! Paper beats Rock"
+    }
+    else {
+      return "You Win! Rock beats Scissors"
+    }
+  }
+  else if (playerSelection == "paper"){
+    if (computerSelection == "rock") {
+      return "You Win! Paper beats Rock"
+    }
+    else if (computerSelection == "paper"){
+      return "Paper Draw"
+    }
+    else {
+      return "You Lose! Scissors beat paper"
+    }
+  }
+  else {
+    if (computerSelection == "rock") {
+      return "You lose! Rock beats Scissors"
+    }
+    else if (computerSelection == "paper") {
+      return "You win! Scissors beat paper"
+    }
+    else {
+      return "Paper Draw"
+    }
+  }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRPS(playerSelection, computerSelection));
