25fbb54e9a26d2d02e598fe6a1a7757799ea9a22
alexandru <109861946+magic-doggo@users.noreply.github.com>
2023-09-30 16:34:01 +0000

    Fix logging, printed values accurately represent return value

--- myScript.js
+++ myScript.js
@@ -1,52 +1,53 @@
 function getComputerChoice(){
   let options = ["rock", "paper", "scissors"];
   let choice = options[Math.floor(Math.random()* options.length)];
-  return choice
+  return choice;
 }
 
-console.log(getComputerChoice())
-
-// function plays a single round of RPS
-// takes 2 PARAMETERS
-// return a string that declares the winner of the round
-// make player parameter case-insensitive
 
 function playRPS (playerSelection, computerSelection){
-  if (playerSelection == "rock"){
+  if (playerSelection.toLowerCase() == "rock"){
     if (computerSelection =="rock") {
-      return "Rock Draw"
+      return "Rock Draw";
     }
     else if (computerSelection == "paper") {
-      return "You Lose! Paper beats Rock"
+      return "You Lose! Paper beats Rock";
     }
     else {
-      return "You Win! Rock beats Scissors"
+      return "You Win! Rock beats Scissors";
     }
   }
-  else if (playerSelection == "paper"){
+  else if (playerSelection.toLowerCase() == "paper"){
     if (computerSelection == "rock") {
-      return "You Win! Paper beats Rock"
+      return "You Win! Paper beats Rock";
     }
     else if (computerSelection == "paper"){
-      return "Paper Draw"
+      return "Paper Draw";
     }
     else {
-      return "You Lose! Scissors beat paper"
+      return "You Lose! Scissors beat paper";
+    }
+  }
+  else if (playerSelection.toLowerCase() == "scissors"){
+    if (computerSelection == "rock") {
+      return "You lose! Rock beats Scissors";
+    }
+    else if (computerSelection == "paper") {
+      return "You win! Scissors beat paper";
+    }
+    else {
+      return "Scissors draw";
     }
   }
   else {
-    if (computerSelection == "rock") {
-      return "You lose! Rock beats Scissors"
-    }
-    else if (computerSelection == "paper") {
-      return "You win! Scissors beat paper"
-    }
-    else {
-      return "Paper Draw"
-    }
+    return "Choose one! Rock, paper or scissors?"
   }
 }
+  
 
-const playerSelection = "rock";
+const playerSelection = "Scissors";
+
 const computerSelection = getComputerChoice();
 console.log(playRPS(playerSelection, computerSelection));
+console.log(playerSelection)
+console.log("pc " + computerSelection)
